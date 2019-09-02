15933bfbbb74ff8a9bbd58af683926ede0916c60
z81 <muzo0@yandex.ru>
2019-09-02 03:27:56 +0300

    upd

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -6,6 +6,8 @@ import { observer } from 'mobx-react';
 import { Button } from '../../components/Button/Button';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { HeaderPages } from './components/HeaderPages';
+import { NewsLoaderPlaceholder } from 'components/App/components/NewsCard/NewsLoaderPlaceholder';
+import { Card } from 'components/Card/Card';
 
 type MainAppProps = { store: NewsListStoreType };
 
@@ -20,6 +22,7 @@ export const MainPage: React.FC<MainAppProps> = observer(({ store }) => {
           <FaArrowRight />
         </Button>
       </HeaderPages>
+      {store.isLoading && [1, 2, 3, 4, 5].map(i => <NewsLoaderPlaceholder key={i} />)}
       {store.news.map(newStore => (
         <NewsCard key={newStore.id} store={newStore} />
       ))}
